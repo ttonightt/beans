b36df17c55e04e99d17780c0e318970f8641aa37
Michael Astashenkov <michaelastash@outlook.com>
2025-11-16 14:23:14 +0100

    Adjusted configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { dirname, resolve } from "node:path"
 import { fileURLToPath } from "node:url"
 import type { UserConfig } from "vite/dist/node"
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -8,8 +9,15 @@ export default {
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.js"),
-			name: "Beans.js",
-			formats: [ "es", "cjs", "umd" ]
+			name: "beans",
+			formats: [ "es", "cjs", "umd" ],
+			fileName: "index"
 		}
-	}
+	},
+
+	plugins: [
+		dts({
+			insertTypesEntry: true
+		})
+	]
 } satisfies UserConfig;
